4f0a3b685ef3bf51dc42c2c1d686f52ab4c0e832
yuntaoyang <uestcyangyuntao@gmail.com>
2021-10-07 18:57:20 -0500

    update

--- heatmap.R
+++ heatmap.R
@@ -2,23 +2,32 @@
 library(ComplexHeatmap)
 library(ggplot2)
 library(readr)
+
 #---- read files ---------------------------------------------------------------
 matrix <- read.csv("matrix.csv", row.names = 1)
 group <- read.csv("group.csv")
+
 #---- set up parameters --------------------------------------------------------
-# color for heatmap
-color <- colorRampPalette(colors = c("blue","white","red"))(100)
-# color for different group
-colAnn <- HeatmapAnnotation(df = data.frame(condition = group$group),
-                            col = list('condition' = c('group_a' = 'blue', 'group_b' = 'red')))
-# legend of heatmap
+condition <- group$group # group information for samples
+color_heatmap <- c("blue","white","red") # color for heatmap
+color_group <- c('group_a' = 'blue', 'group_b' = 'red') # color for different group
+file_name <- 'example.pdf' # file name
+width <- 8 # width of plot
+height <- 10 # height of plot
+show_row_names <- FALSE # show gene names or not
+cluster_rows <- FALSE # cluster genes or not
+cluster_columns <- FALSE # cluster samples of not
+
+#---- heatmap ------------------------------------------------------------------
+color <- colorRampPalette(colors = color_heatmap)(100)
+colAnn <- HeatmapAnnotation(df = data.frame(condition = condition),
+                            col = list('condition' = color_group))
 heatmap_legend = list(title= "Z-Score(logCPM)", title_position = "topcenter", legend_direction="horizontal")
-# make heatmap
-pdf('test.pdf',width = 8, height = 10)
+pdf(file_name,width = width, height = height)
 p <- Heatmap(as.matrix(matrix), 
-            show_row_names = FALSE, # show gene name
-            cluster_rows = FALSE, # clustering in genes
-            cluster_columns = FALSE, #clustering in samples
+            show_row_names = show_row_names,
+            cluster_rows = cluster_rows, 
+            cluster_columns = cluster_columns,
             col = color, 
             top_annotation=colAnn,
             heatmap_legend_param = heatmap_legend)
