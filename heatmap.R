2a1741fd5465ebdb968aac4c94ff2cfae63b8795
yuntaoyang <uestcyangyuntao@gmail.com>
2022-03-11 09:49:05 -0600

    fix

--- heatmap.R
+++ heatmap.R
@@ -18,19 +18,26 @@ show_row_names <- FALSE # show gene names or not
 cluster_rows <- FALSE # cluster genes or not
 cluster_columns <- FALSE # cluster samples of not
 
+#---- heatmap function ---------------------------------------------------------
+heatmap <- function(matrix,condition,color_heatmap,color_group,file_name,width,height,show_row_names,cluster_rows,cluster_columns)
+{
+  color <- colorRampPalette(colors = color_heatmap)(100)
+  colAnn <- HeatmapAnnotation(df = data.frame(condition = condition),
+                              col = list('condition' = color_group))
+  heatmap_legend = list(title= "Z-Score(logCPM)", title_position = "topcenter", legend_direction="horizontal")
+  pdf(file_name,width = width, height = height)
+  p <- Heatmap(as.matrix(matrix), 
+               show_row_names = show_row_names,
+               cluster_rows = cluster_rows, 
+               cluster_columns = cluster_columns,
+               col = color, 
+               top_annotation=colAnn,
+               heatmap_legend_param = heatmap_legend)
+  draw(p, merge_legend = FALSE, heatmap_legend_side = "bottom",annotation_legend_side = "right")
+  dev.off()
+}
+
 #---- heatmap ------------------------------------------------------------------
-color <- colorRampPalette(colors = color_heatmap)(100)
-colAnn <- HeatmapAnnotation(df = data.frame(condition = condition),
-                            col = list('condition' = color_group))
-heatmap_legend = list(title= "Z-Score(logCPM)", title_position = "topcenter", legend_direction="horizontal")
-pdf(file_name,width = width, height = height)
-p <- Heatmap(as.matrix(matrix), 
-            show_row_names = show_row_names,
-            cluster_rows = cluster_rows, 
-            cluster_columns = cluster_columns,
-            col = color, 
-            top_annotation=colAnn,
-            heatmap_legend_param = heatmap_legend)
-draw(p, merge_legend = FALSE, heatmap_legend_side = "bottom",annotation_legend_side = "right")
-dev.off()
+heatmap(matrix,condition,color_heatmap,color_group,file_name,width,height,show_row_names,cluster_rows,cluster_columns)
+
 
